6f306ac7e3fcf599bdc48051d4b7e8236af3cf5e
quintojessy <quintojessy@gmail.com>
2025-07-28 17:52:36 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,25 +3,34 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+
+  # Use https://search.nixos.org/packages to find packages.
+  # We are ensuring git and git-filter-repo are both included.
   packages = [
-    pkgs.nodejs_20,
-    # Adding git-filter-repo to clean the git history
+    pkgs.nodejs_20
+    pkgs.git
     pkgs.git-filter-repo
   ];
-  # Sets environment variables in the workspace
+
+  # Sets environment variables in the workspace.
   env = {};
+
+  # IDX-specific configuration.
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id".
     extensions = [
+      # e.g., "vscodevim.vim"
     ];
+
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
+      # Runs when a workspace is first created with this `dev.nix` file.
       onCreate = {
-        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress";
       };
-      # Runs when a workspace is (re)started
+
+      # Runs when a workspace is (re)started.
       onStart = {
+        # The 'npm run dev' script is defined in package.json
         run-server = "npm run dev";
       };
     };
